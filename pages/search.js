a803ebf35f310d97eb528cc918ee12fbc520ff5f
Ebraj Grg <gurungebraj22@gmail.com>
2022-01-15 20:56:22 +0545

    done-with-server-side-rendering

--- pages/search.js
+++ pages/search.js
@@ -2,8 +2,10 @@ import { useRouter } from "next/router";
 import { format } from "date-fns";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import InfoCard from "../components/InfoCard";
 
-function Search() {
+function Search({ searchResults }) {
+  console.log(searchResults);
   const router = useRouter();
   const { location, startDate, endDate, noOfGuests } = router.query;
   console.log(location, startDate, endDate, noOfGuests);
@@ -20,12 +22,29 @@ function Search() {
               300+ Stays - {range} - {noOfGuests} for guests
             </p>
             <h2 className="text-2xl font-bold py-2">Stays in {location}</h2>
-            <div className="hidden lg:inline-flex mb-4 space-x-1">
+            <div className="hidden lg:inline-flex my-4 space-x-1">
               <p className="button">Cancellation Flexibility</p>
               <p className="button">Type of Place</p>
               <p className="button">Price</p>
               <p className="button">Rooms & Beds</p>
               <p className="button">More filters</p>
+            </div>
+
+            <div>
+              {searchResults.map(
+                ({ img, location, title, description, star, price, total }) => (
+                  <InfoCard
+                    key={img}
+                    img={img}
+                    location={location}
+                    title={title}
+                    description={description}
+                    star={star}
+                    price={price}
+                    total={total}
+                  />
+                )
+              )}
             </div>
           </section>
         </div>
@@ -36,3 +55,15 @@ function Search() {
 }
 
 export default Search;
+
+export async function getServerSideProps() {
+  const searchResults = await fetch("https://links.papareact.com/isz").then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      searchResults,
+    },
+  };
+}
